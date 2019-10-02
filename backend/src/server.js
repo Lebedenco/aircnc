cf535bb1c193b725b358daf42c196d2af24d444a
Lebedenco <diegolebedenco1@gmail.com>
2019-10-02 20:40:39 -0300

    Finished frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+
 const routes = require("./routes");
 const config = require("./config/config.json");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3333;
 const url = config.db.url;
 
 mongoose.connect(url, { 
@@ -12,7 +15,9 @@ mongoose.connect(url, {
   useUnifiedTopology: true
  });
 
+app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 app.listen(port, () => {
